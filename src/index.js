63c16ff6326f93d458253bf8bc4f16788543dd5b
Jakel Alonso <alonsojakel60@gmail.com>
2023-12-01 22:50:30 -0500

    sidebar fixed

--- src/index.js
+++ src/index.js
@@ -9,24 +9,13 @@ async function loadSidebar() {
 }
 
 window.onload = loadSidebar;
-// Function to load HTML content from a file
-function loadContent(url, containerId) {
-    fetch(url)
-        .then(response => response.text())
-        .then(data => document.getElementById(containerId).innerHTML = data)
-        .catch(error => console.error('Error:', error));
-}
-
-// Load content of otherpage.html into the content-container div
-loadContent('sidebar.html', 'content2');
-loadContent('topbar.html','content1');
 
 document.getElementById('submitButton').addEventListener('click', function() {
-    // Get all forms
-    var forms = document.querySelectorAll('.partNum, .location, .vendor, .manu');
+        // Get all forms
+        var forms = document.querySelectorAll('.partNum, .location, .vendor, .manu');
 
-    // Submit each form
-    forms.forEach(function(form) {
-        form.submit();
+        // Submit each form
+        forms.forEach(function(form) {
+            form.submit();
+        });
     });
-});
