ead90ebdc29b9f3a89a83de4336bf7fe3e3e37b4
Wilber Cortez <Wilbercortez2013@gmail.com>
2023-12-02 01:21:51 -0500

    added CSV functionality

--- backend/csv_parsing.js
+++ backend/csv_parsing.js
@@ -7,3 +7,19 @@ const inventoryCRUD = require('CRUD/inventory_entries.js');
 //CSV parsing functions
 //CSV FORMAT 
 //Part Prefix | Part Number | Part Type	| Warehouse	| Zone | Quantity | Condition | Unit Cost| Entry Notes| Vendor Name | Manufacturer | Date
+
+function parseCSV(filePath) {
+    return new Promise((resolve, reject) => {
+        const results = [];
+        
+        fs.createReadStream(filePath)
+            .pipe(csv())
+            .on('data', (data) => results.push(data))
+            .on('end', () => {
+                resolve(results);
+            })
+            .on('error', reject);
+    });
+}
+
+
---
+++ src/autoAddEntry.js
@@ -0,0 +1,69 @@
+const { ipcRenderer } = require('electron');
+const csv_parsing = require('./backend/csv_parsing.js');
+
+document.addEventListener('DOMContentLoaded', () => {
+    const dropZone = document.getElementById('drop-zone');
+    const fileInput = document.getElementById('fileInput');
+    const fileSelectBtn = document.getElementById('fileSelectBtn');
+    const addButton = document.getElementById('addButton');
+
+    dropZone.addEventListener('dragover', (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        // Add style to drop zone
+        dropZone.classList.add('dragover');
+    });
+
+    dropZone.addEventListener('dragleave', (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        // Remove style from drop zone
+        dropZone.classList.remove('dragover');
+    });
+
+    dropZone.addEventListener('drop', (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        dropZone.classList.remove('dragover');
+        const file = event.dataTransfer.files[0];
+        handleFile(file);
+    });
+
+    fileSelectBtn.addEventListener('click', () => {
+        fileInput.click();
+    });
+
+    fileInput.addEventListener('change', () => {
+        const file = fileInput.files[0];
+        handleFile(file);
+    });
+
+    addButton.addEventListener('click', () => {
+        // Trigger IPC event to main process to add the inventory entry
+        ipcRenderer.send('add-inventory-entry', {/* CSV Data or file path */});
+    });
+});
+
+function handleFile(file) {
+    if (!file || file.type !== 'text/csv') {
+        alert('Please provide a CSV file.');
+        return;
+    }
+
+    // Read the CSV file
+    let csvData = csv_parsing.parseCSV(file.path);
+    // Send the CSV data to main process via IPC for further processing
+    ipcRenderer.send('process-csv', csvData);
+}
+
+
+// Listening for the response from main process
+ipcRenderer.on('process-csv-response', (event, { success, error }) => {
+    if (success) {
+        console.log('CSV processed successfully');
+        // Do something on success
+    } else {
+        console.error('Error processing CSV:', error);
+        // Display error message to the user
+    }
+});
